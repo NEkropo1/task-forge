f3b647f99a8dd8d36ad03add8b28cd9512e6e569
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-03 00:40:20 +0300

    debug back to ultimate value

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-tzr8rmhc9d9y!1jk_0124-fme1_kc)e5ce=u+n^fw^33*5*c02")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
