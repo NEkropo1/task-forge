ee91266268effc354cb3aa2316e01747129d00cf
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-03 08:38:39 +0300

    make custom completion filter more intuitive

--- forge/templatetags/custom_filters.py
+++ forge/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core.exceptions import ValidationError
 from django.db.models import QuerySet
 
 register = template.Library()
@@ -8,4 +9,6 @@ register = template.Library()
 def filter_by_completion(queryset: QuerySet, condition: str) -> QuerySet:
     if condition == "+":
         return queryset.filter(is_completed=True)
-    return queryset.filter(is_completed=False)
+    if condition == "-":
+        return queryset.filter(is_completed=False)
+    raise ValidationError("Invalid condition. Must be '+' or '-'.")
